708433c1c78fba97685e485ff2938c5d7a746ab9
Learnoset - Learn Coding Online <info.learnoset@gmail.com>
2021-11-22 16:17:05 +0530

    New Released

---
+++ LearnosetNavigationBar/src/main/java/com/learnoset/material/ui/learnosetnavigationbar/LearnosetExceptions.java
@@ -0,0 +1,8 @@
+package com.learnoset.material.ui.learnosetnavigationbar;
+
+public class LearnosetExceptions extends Throwable {
+
+    public LearnosetExceptions(String message){
+        super(message);
+    }
+}
--- LearnosetNavigationBar/src/main/java/com/learnoset/material/ui/learnosetnavigationbar/LearnosetNavigationBar.java
+++ LearnosetNavigationBar/src/main/java/com/learnoset/material/ui/learnosetnavigationbar/LearnosetNavigationBar.java
@@ -1,16 +1,23 @@
 package com.learnoset.material.ui.learnosetnavigationbar;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 
 import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -30,6 +37,51 @@ public class LearnosetNavigationBar extends NavigationView {
 
     public void enableDarkMode(boolean isDarkModeEnabled) {
         darkModeEnabled = isDarkModeEnabled;
+    }
+
+    public void initializeHeader(String profileImageUrl, String profileNameTxt) {
+        Picasso.get().load(profileImageUrl).into(headerImage);
+        profileName.setText(profileNameTxt);
+    }
+
+    public void initializeHeader(File profileImageFile, String profileNameTxt) throws LearnosetExceptions {
+        profileName.setText(profileNameTxt);
+
+        if (profileImageFile.exists()) {
+            Uri imageUri = FileProvider.getUriForFile(
+                    context,
+                    "com.learnoset.fileprovider",
+                    profileImageFile);
+            headerImage.setImageURI(imageUri);
+        } else {
+            throw new LearnosetExceptions("Invalid File Path");
+        }
+    }
+
+    public void initializeHeader(Uri profileImageUri, String profileNameTxt) throws LearnosetExceptions {
+        profileName.setText(profileNameTxt);
+
+        final ContentResolver contentResolver = context.getContentResolver();
+        final String[] projection = {MediaStore.MediaColumns.DATA};
+
+        final Cursor cursor = contentResolver.query(profileImageUri, projection, null, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                String filePath = cursor.getString(0);
+
+                if (new File(filePath).exists()) {
+                    headerImage.setImageURI(profileImageUri);
+                } else {
+                    throw new LearnosetExceptions("No File Found");
+                }
+            } else {
+                throw new LearnosetExceptions("Invalid URI Format");
+            }
+            cursor.close();
+        } else {
+            throw new LearnosetExceptions("Invalid URI");
+        }
+
     }
 
     @SuppressLint("InflateParams")
